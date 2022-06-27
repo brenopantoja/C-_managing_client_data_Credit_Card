102f5f85e61c6408cd362b61035f36eabf79904a
Breno Pantoja <breno.r.pantoja@gmail.com>
2022-06-27 12:32:50 -0300

    270502022

---
+++ RDI Credit Card/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace RDI_Credit_Card
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textCreditCard = new System.Windows.Forms.TextBox();
+            this.textDate = new System.Windows.Forms.TextBox();
+            this.textCvv = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Credit Card Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "M/Year:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CVV:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(122, 154);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Ok";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textCreditCard
+            // 
+            this.textCreditCard.Location = new System.Drawing.Point(27, 37);
+            this.textCreditCard.Name = "textCreditCard";
+            this.textCreditCard.Size = new System.Drawing.Size(170, 23);
+            this.textCreditCard.TabIndex = 5;
+            this.textCreditCard.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textCreditCard_KeyPress);
+            // 
+            // textDate
+            // 
+            this.textDate.Location = new System.Drawing.Point(27, 81);
+            this.textDate.Name = "textDate";
+            this.textDate.Size = new System.Drawing.Size(48, 23);
+            this.textDate.TabIndex = 6;
+            this.textDate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textDate_KeyPress);
+            // 
+            // textCvv
+            // 
+            this.textCvv.Location = new System.Drawing.Point(27, 125);
+            this.textCvv.Name = "textCvv";
+            this.textCvv.Size = new System.Drawing.Size(75, 23);
+            this.textCvv.TabIndex = 7;
+            this.textCvv.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textCvv_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 189);
+            this.Controls.Add(this.textCvv);
+            this.Controls.Add(this.textDate);
+            this.Controls.Add(this.textCreditCard);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textCreditCard;
+        private System.Windows.Forms.TextBox textDate;
+        private System.Windows.Forms.TextBox textCvv;
+    }
+}
+
---
+++ RDI Credit Card/Form1.cs
@@ -0,0 +1,265 @@
+﻿using Rhino.Mocks.Constraints;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace RDI_Credit_Card
+{
+    public partial class Form1 : Form
+    {
+      
+
+        public static string cardNumber { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            limparTela();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //they are number max
+            const long textcardNumber = 16;
+            const int textcardVal = 5;
+            const int textcardDate = 4;
+
+
+         
+            if (textCreditCard.Text.Length < textcardNumber)
+            {
+                MessageBox.Show("Número do cartão de Credito tem que ser 16 Digitos !");
+            }
+
+            if (textCvv.Text.Length < textcardVal && textCvv.Text.Length <3 ) {
+
+                MessageBox.Show("Número do código de segurança é de 5 digitos: Pode está entre 3 à 5 digitos !");
+            
+            }
+
+
+            if (textDate.Text.Length < textcardDate) {
+
+                MessageBox.Show("Número de Validade do Cartão de Credito são 4 Digitos (Mês/Ano:)");
+
+            }
+
+            else {
+
+                validarCampo();
+                MessageBox.Show("Transação realizada com Sucesso !!"+ "\nNúmero de seu cartão é: " +textCreditCard.Text);
+                // It has calling the Last Digit of Credit Card method 
+                ultimosdigitosCartao();
+                tipodoCartao();
+                stringRevers();
+
+            }
+
+            limparTela();
+
+        }
+
+        private void  stringRevers()
+        {
+            //It has doing number credit card reverse;
+            string datarevers = textCvv.Text;
+
+            
+            string stringArray = datarevers;
+            string reverse = string.Empty;
+            for (int i = stringArray.Length - 1; i >= 0; i--)
+            {
+                reverse += stringArray[i];
+            }
+            MessageBox.Show("Número do Codigo CVV, reverso cartão: Primeira rotação é= " + reverse);
+            
+
+            
+            //It has doing number credit card reverse:
+            string datarevers2 = textCreditCard.Text;
+
+            
+            string stringArray2 = reverse;
+            string reverse2 = string.Empty;
+
+            for (int i = stringArray2.Length - 1; i >= 0; i--)
+            {
+                reverse2 += stringArray2[i];
+            }
+            MessageBox.Show("Número do Codigo CVV: Segunda rotação é= " + reverse2);
+        }
+
+        private void tipodoCartao() {
+          //  It has taking element first of the string
+            string first = textCreditCard.Text.Substring(0, 1);
+
+            MessageBox.Show("Número de seu cartão é: " + first);
+            /*
+             * 3 – cartões de viagem / entretenimento ( American Express e Diners Club)
+
+                4 – Visa
+
+                5 – MasterCard
+
+                6 – Discover Card
+             */
+            
+
+            if (first == "3")
+            {
+                MessageBox.Show("Número de seu cartão de American Express ! " + "Primeiro digito: "+first);
+
+            }
+
+                
+            if (first == "4") {
+                MessageBox.Show("Número de seu cartão de Bandeira Visa ! "+ "Primeiro digito: "+first);
+
+
+            }
+            if (first == "5")
+            {
+                MessageBox.Show("Número de seu cartão de Bandeira MasterCard! " + "Primeiro digito: "+first);
+            }
+            if (first == "6")
+            {
+                MessageBox.Show("Número de seu cartão de Bandeira Discover Card !" + "Primeiro digito: " + first);
+            }
+
+
+        }
+
+        private void limparTela() {
+            textCreditCard.Text = string.Empty;
+            textCvv.Text = string.Empty;
+            textDate.Text = string.Empty;
+        }
+        private void ultimosdigitosCartao() {
+            string lastDigit = textCreditCard.Text;
+            //It has taking last digit credit card
+            lastDigit.Substring(lastDigit.Length - 4);
+            MessageBox.Show("Os últimos digitos de seu cartão são: " + lastDigit.Substring(lastDigit.Length - 4));
+
+        }
+
+        private void validarCampo() {
+
+            if (String.IsNullOrEmpty(textCreditCard.Text))
+            {
+                MessageBox.Show("Faltou digitar: O número do cartão de credito");
+
+
+            }
+
+            if (String.IsNullOrEmpty(textCvv.Text))
+            {
+                MessageBox.Show("Faltou digitar: O número do código de segurança");
+            }
+
+            if (String.IsNullOrEmpty(textDate.Text))
+            {
+                MessageBox.Show("Faltou digitar: A validade do cartão");
+            }
+
+            else
+            {
+                exportandoExcel();
+               
+
+            }
+
+
+
+        }
+    
+
+        private void exportandoExcel() {
+
+            string first = textCreditCard.Text.Substring(0, 1);
+
+
+            // It has taking fields of the credit card and it has saving in cvs file
+            var dadosCartao = "Card Credit Number:" +
+                textCreditCard.Text +
+                "; Card Credit Date: " +
+                textDate.Text + "; Code Card Credit:" + textCvv.Text +
+                ";Last Digit Credit Card: " +
+                textCreditCard.Text.Substring(textCreditCard.Text.Length - 4) + "; Tipo de Cartao: " + first +
+                "; Bank Time Operation: " + DateTime.Now.ToString();//It has taking on time now
+
+
+            //It has working: It has saving it cvs file
+            using (StreamWriter file = new StreamWriter(@"C:\dadoscharp\dados.csv"))
+            {
+                string[] lines = { "New line 1", "New line 2" };
+                file.WriteLine(dadosCartao, lines);
+
+                string text = "First line" + Environment.NewLine;
+
+               
+            }
+        }
+
+        private void textCreditCard_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Just it has aceptting number
+            if (!char.IsDigit(e.KeyChar))
+            {
+                //It has doing cancel the event
+                e.Handled = true;
+            }
+        }
+
+        private void textDate_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Just it has aceptting number
+            if (!char.IsDigit(e.KeyChar))
+            {
+                //It has doing cancel the event
+                e.Handled = true;
+            }
+        }
+
+        private void textCvv_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Just it has aceptting number of the code credit cart
+            if (!char.IsDigit(e.KeyChar))
+            {
+                //It has doing cancel the event of the code credit cart
+                e.Handled = true;
+            }
+        }
+        
+
+    }
+
+}
